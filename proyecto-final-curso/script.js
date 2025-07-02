b072e126902007ffe884b685fe53384ddab89d2e
Machuga <cafrunimateo@gmail.com>
2025-07-02 08:25:13 -0300

    Este rama tiene agregado js

---
+++ proyecto-final-curso/script.js
@@ -0,0 +1,36 @@
+const menu = document.querySelector('#menu-icon')
+const navList = document.querySelector('.navlist')
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x')
+    navList.classList.toggle('open')
+}
+
+const scrollReveal = ScrollReveal({
+    distance: '65px',
+    duration: 2000,
+    delay: 450,
+    reset: true,
+})
+
+scrollReveal.reveal('.hero-text', {delay: 200, origin: 'right'})
+scrollReveal.reveal('.hero-img', {delay: 300, origin: 'bottom'})
+scrollReveal.reveal('.icons', {delay: 180, origin: 'left'})
+scrollReveal.reveal('.scroll-down', {delay: 500, origin: 'top'})
+scrollReveal.reveal('.navlist', {delay: 200, origin: 'top'})
+
+const sDown = document.querySelector('.scroll-down')
+sDown.onclick = () => {
+    const namePrompt = prompt('Su nombre completo:')
+    const genero = confirm('¿Sos hombre?')
+    document.querySelector('img').remove()
+    if (!genero) {
+        document.querySelector('h5').innerHTML = 'Hija del Altisimo'
+        document.querySelector('h4').innerHTML = 'Salva por gracia❤️‍🔥'
+        
+    }
+    if (namePrompt) {
+        document.querySelector('h1').innerHTML = namePrompt;
+    }
+    
+}
